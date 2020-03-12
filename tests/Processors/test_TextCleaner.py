7a2aba08e52aa6785e24ca02a61de432408e2ca8
AdamSwenson <adam.swenson@csun.edu>
2020-03-12 12:07:52 -0700

    progress on CAN-59 Have a basic framework to run cleaners. Still need to actually run on stuff

--- tests/Processors/test_TextCleaner.py
+++ tests/Processors/test_TextCleaner.py
@@ -11,6 +11,16 @@ from TestingBase import TestingBase
 if __name__ == '__main__':
     pass
 
+TEST_DATA =[ {'input': '<div><span="dog">¬†¬†¬†¬†¬†¬†¬†¬†¬†¬†¬†</span></div>',
+              'expect': ''
+              },
+             {'input': '<div><span="dog">Dog breath is taco-like.¬†¬† Hmmm¬†¬†¬†¬†¬†¬†¬†¬†¬†</span></div>',
+              'expect': 'Dog breath is taco-like. Hmmm'
+              },
+             {'input': "Dog breath is taco-like. Hmmm",
+              'expect': "Dog breath is taco-like. Hmmm"
+              }
+             ]
 
 class TestTextCleaner( TestingBase ):
 
@@ -19,6 +29,7 @@ class TestTextCleaner( TestingBase ):
         self.config_for_test()
         self.obj = TextCleaner()
 
-
     def test_clean( self ):
-        self.fail()
+        for t in TEST_DATA:
+            self.assertEqual(t['expect'], self.obj.clean(t['input']))
+
