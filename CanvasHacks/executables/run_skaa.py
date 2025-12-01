83985f7bd0c441a8faf7177f952309bd5628ad5d
adam swenson <adam.swenson@csun.edu>
2025-12-01 13:13:21 -0800

    created executable file for running skaa from command line

---
+++ CanvasHacks/executables/run_skaa.py
@@ -0,0 +1,43 @@
+"""This replicates the control panel for running at the command line"""
+
+from CanvasHacks.Displays.dashboard import ControlStore
+from CanvasHacks.executables.easy_skaa_runner import run_multiple_units
+
+
+START_UNIT = 1
+STOP_UNIT = 7
+# MINUTES_REST = 120
+MINUTES_REST = 45
+
+
+REPORT_GEN_TIMEOUT = 25#60
+REPORT_ID_ATTEMPTS = 100
+
+# import os
+
+# path = "/".join([a for a in os.path.abspath("").split('/') if a not in ['Notebooks', 'personal']])
+# %cd $path
+
+# from CanvasHacks import environment
+
+import sys
+
+def main(start_unit=START_UNIT, stop_unit=STOP_UNIT, minutes_rest=MINUTES_REST):
+    control_store = ControlStore()
+
+    try:
+        run_multiple_units(control_store, start_unit, stop_unit, minutes_rest, rest_timeout=REPORT_GEN_TIMEOUT,
+                           max_id_attempts=REPORT_ID_ATTEMPTS)
+    except:
+        run_multiple_units(control_store, start_unit, stop_unit, minutes_rest, rest_timeout=REPORT_GEN_TIMEOUT,
+                           max_id_attempts=REPORT_ID_ATTEMPTS)
+
+
+if __name__ == '__main__':
+    start_unit = sys.argv[0] if sys.argv[0] is not None else START_UNIT
+    stop_unit = sys.argv[1] if sys.argv[1] is not None else STOP_UNIT
+    minutes_rest = sys.argv[2] if sys.argv[2] is not None else MINUTES_REST
+
+    main(start_unit, stop_unit, minutes_rest)
+
+
