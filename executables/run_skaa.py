d7fb324b9a0bdd2d0b8b1271c47f0a5855af7245
adam swenson <adam.swenson@csun.edu>
2025-12-01 16:06:07 -0800

    command line executable seems to be working

--- executables/run_skaa.py
+++ executables/run_skaa.py
@@ -1,8 +1,13 @@
 """This replicates the control panel for running at the command line"""
 
+import sys
+import os
+
+path = "/".join([a for a in os.path.abspath("").split('/') if a not in ['Notebooks', 'personal']])
+sys.path.append(path)
+
 from CanvasHacks.Displays.dashboard import ControlStore
 from CanvasHacks.executables.easy_skaa_runner import run_multiple_units
-
 
 START_UNIT = 1
 STOP_UNIT = 7
@@ -13,17 +18,14 @@ MINUTES_REST = 45
 REPORT_GEN_TIMEOUT = 25#60
 REPORT_ID_ATTEMPTS = 100
 
-# import os
-
-# path = "/".join([a for a in os.path.abspath("").split('/') if a not in ['Notebooks', 'personal']])
-# %cd $path
-
-# from CanvasHacks import environment
-
-import sys
 
 def main(start_unit=START_UNIT, stop_unit=STOP_UNIT, minutes_rest=MINUTES_REST):
+    path = "/".join([a for a in os.path.abspath("").split('/') if a not in ['Notebooks', 'personal']])
+    os.chdir(path)
+
     control_store = ControlStore()
+
+    print(f"Running for unit {start_unit} to {stop_unit}...")
 
     try:
         run_multiple_units(control_store, start_unit, stop_unit, minutes_rest, rest_timeout=REPORT_GEN_TIMEOUT,
@@ -34,10 +36,14 @@ def main(start_unit=START_UNIT, stop_unit=STOP_UNIT, minutes_rest=MINUTES_REST):
 
 
 if __name__ == '__main__':
-    start_unit = sys.argv[0] if sys.argv[0] is not None else START_UNIT
-    stop_unit = sys.argv[1] if sys.argv[1] is not None else STOP_UNIT
-    minutes_rest = sys.argv[2] if sys.argv[2] is not None else MINUTES_REST
+    print(sys.argv)
+    try:
+        start_unit = int(sys.argv[1]) if sys.argv[1] is not None else START_UNIT
+        stop_unit = int(sys.argv[2]) if sys.argv[2] is not None else STOP_UNIT
+        # minutes_rest = sys.argv[3] if sys.argv[3] is not None else MINUTES_REST
+    except IndexError:
+        pass
 
-    main(start_unit, stop_unit, minutes_rest)
+    main(start_unit, stop_unit)
 
 
